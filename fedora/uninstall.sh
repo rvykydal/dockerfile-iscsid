3dbeff921abdc1301ff80463829d46d1431c34cf
Radek Vykydal <rvykydal@redhat.com>
2015-03-30 17:56:16 +0200

    Make uninstall.sh a bit more sane.

--- fedora/uninstall.sh
+++ fedora/uninstall.sh
@@ -2,8 +2,6 @@
 
 prefix=${HOST:-/host}
 
-rm -rf ${prefix}/etc/iscsi/iscsid.conf
-rm -rf ${prefix}/etc/iscsi/initiatorname.iscsi
-rm -rf ${prefix}/var/lock/iscsi/lock
-rmdir ${prefix}/etc/iscsi
-rmdir ${prefix}/var/lock/iscsi
+rm -rf ${prefix}/etc/iscsi
+rm -rf ${prefix}/var/lock/iscsi
+rm -rf ${prefix}/var/lib/iscsi
