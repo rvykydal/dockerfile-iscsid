0239b08baf797454073b4af25500d79a6dfed5d5
Radek Vykydal <rvykydal@redhat.com>
2015-03-30 15:09:38 +0200

    Remove also /etc/iscsi in uninstall.

--- rhel7/uninstall.sh
+++ rhel7/uninstall.sh
@@ -5,4 +5,5 @@ prefix=${HOST:-/host}
 rm -rf ${prefix}/etc/iscsi/iscsid.conf
 rm -rf ${prefix}/etc/iscsi/initiatorname.iscsi
 rm -rf ${prefix}/var/lock/iscsi/lock
+rmdir ${prefix}/etc/iscsi
 rmdir ${prefix}/var/lock/iscsi
